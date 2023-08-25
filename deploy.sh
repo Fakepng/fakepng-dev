dd12b44e5df7c86ae4ff1ef84fbbd1d6e536eacb
Fakepng <krit0kasemtewin@gmail.com>
2023-08-25 18:31:54 +0700

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+
+echo "Kill all the running PM2 actions"
+sudo pm2 kill
+
+echo "Jump to app folder"
+cd /home/ubuntu/fakepng-dev
+
+echo "Update app from Git"
+git pull
+
+echo "Install app dependencies"
+sudo rm -rf node_modules package-lock.json
+sudo npm install
+
+echo "Build your app"
+sudo npm run build
+
+echo "Run new PM2 action"
+sudo pm2 start npm --name fakepng-dev -- run start -- -p 3000
