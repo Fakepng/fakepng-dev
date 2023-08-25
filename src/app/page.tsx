ce5429c1fe45e0f9e32fcb2283f7d3a0c5022b3a
Fakepng <krit0kasemtewin@gmail.com>
2023-08-26 04:40:41 +0700

    Coming soon...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="">Hello World</h1>
-      fakepng.dev fakepng.com krithub.com
+    <main className="flex h-screen bg-black w-screen">
+      <h1 className="text-slate-100 font-extrabold text-6xl m-auto">
+        <div className="waviy">
+          <span style={{ "--i": 1 } as React.CSSProperties}>c</span>
+          <span style={{ "--i": 2 } as React.CSSProperties}>o</span>
+          <span style={{ "--i": 3 } as React.CSSProperties}>m</span>
+          <span style={{ "--i": 4 } as React.CSSProperties}>i</span>
+          <span style={{ "--i": 5 } as React.CSSProperties}>n</span>
+          <span style={{ "--i": 6 } as React.CSSProperties}>g</span>{" "}
+          <span style={{ "--i": 8 } as React.CSSProperties}>s</span>
+          <span style={{ "--i": 9 } as React.CSSProperties}>o</span>
+          <span style={{ "--i": 10 } as React.CSSProperties}>o</span>
+          <span style={{ "--i": 11 } as React.CSSProperties}>n</span>
+          <span style={{ "--i": 12 } as React.CSSProperties}>.</span>
+          <span style={{ "--i": 13 } as React.CSSProperties}>.</span>
+          <span style={{ "--i": 14 } as React.CSSProperties}>.</span>
+        </div>
+      </h1>
     </main>
   );
 }
